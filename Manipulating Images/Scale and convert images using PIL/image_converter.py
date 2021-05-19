f12a5947fe193e1f382dae2163a7defe6a779890
Kojijo <52007115+Kojijo@users.noreply.github.com>
2021-05-19 22:58:11 +0300

    Update image_converter.py

--- Manipulating Images/Scale and convert images using PIL/image_converter.py
+++ Manipulating Images/Scale and convert images using PIL/image_converter.py
@@ -1,1 +1,10 @@
 
+#!/usr/bin/env python3
+import os
+from PIL import Image
+old_path = os.path.expanduser('~') + '/images/'
+new_path = '/opt/icons/'
+for image in os.listdir(old_path):
+        img = Image.open(old_path + image)
+        img.rotate(-90).resize((128, 128)).convert("RGB").save(new_path + image + '.jpeg')
+        img.close()
